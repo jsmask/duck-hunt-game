936893c9eeec5a0ca60aa13dd68bf2417061369a
mashikai <2674800533@qq.com>
2022-02-21 12:57:29 +0800

    update

--- src/components/DuckGame/js/assets.js
+++ src/components/DuckGame/js/assets.js
@@ -1,12 +1,12 @@
 import { getImageUrl } from "./tools"
 
 const audioList = {
-    fire:getImageUrl("fire","mp3"),
-    caught_duck:getImageUrl("caught_duck","mp3"),
-    bgm:getImageUrl("bgm","mp3"),
-    dog_start:getImageUrl("dog_start","mp3"),
-    dog_laugh:getImageUrl("dog_laugh","mp3"),
-    win:getImageUrl("win","mp3"),
+    fire: getImageUrl("fire", "mp3"),
+    caught_duck: getImageUrl("caught_duck", "mp3"),
+    bgm: getImageUrl("bgm", "mp3"),
+    dog_start: getImageUrl("dog_start", "mp3"),
+    dog_laugh: getImageUrl("dog_laugh", "mp3"),
+    win: getImageUrl("win", "mp3"),
 }
 
 const bird0 = getImageUrl("bird0");
@@ -17,11 +17,11 @@ const stage = getImageUrl("stage");
 
 const dogList = {}
 for (let i = 0; i < 12; i++) {
-    dogList["dog"+i] =  getImageUrl("dog"+ i)
+    dogList["dog" + i] = getImageUrl("dog" + i)
 }
 const duckList = {}
 for (let i = 0; i < 11; i++) {
-    duckList["duck_"+i] =  getImageUrl("duck/"+ i)
+    duckList["duck_" + i] = getImageUrl("duck_" + i)
 }
 
 export default {
