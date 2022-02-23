bad2f60709d1ef2a7123053e1d97f17f65dea78e
mashikai <2674800533@qq.com>
2022-02-23 11:21:34 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Bus from "./utils/bus"
+// import checkDebug from "./utils/checkDebug"
+
+
+// if(import.meta.env.MODE==="production"){
+//     checkDebug()
+// }
 
 const app = createApp(App);
 app.config.globalProperties.$bus = Bus;
---
+++ src/utils/checkDebug.js
@@ -0,0 +1,4 @@
+export default function checkDebug(){
+    debugger; 
+    window.requestAnimationFrame(checkDebug)
+}
